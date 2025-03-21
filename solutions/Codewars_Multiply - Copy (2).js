d230eef90c0a556af6143d0051538813d4f9462e
Mohamed El-Fiky <fiky.link@gmail.com>
2025-03-21 22:17:32 +0200

    Add solution #17

---
+++ solutions/Codewars_Multiply - Copy (2).js
@@ -0,0 +1,15 @@
+/*
+    Link: https://www.codewars.com/kata/50654ddff44f800200000004/
+
+    Description:
+
+        Simple multiply function
+
+
+
+*/
+
+
+function multiply(a, b){
+    return  a * b
+  }
